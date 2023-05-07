9193cf28c980abee1d33d420d93bafdcb5c1822d
a-128 <128114816+a-128@users.noreply.github.com>
2023-05-07 14:41:11 +0100

    Create clientedits.js

---
+++ clientedits.js
@@ -0,0 +1,12 @@
+pl1=Deobfuscator.object(ig.game, "addForumThreadAndFirstComment_P", true);
+pl2=Deobfuscator.function(ig.game[pl1], "/j/u/p/", true);
+map=Deobfuscator.object(ig.game,'queuePerformDelayMs',true);
+editswit=false
+setmap22=ig.game[map].setMap
+eval("setmap23="+ig.game[map].setMap.toString().replaceAll("d||", "d&&"))
+ig.game[pl1][pl2]=function() {};
+setInterval(()=>{
+if (ig.game[pl1][pl2].toString()=='function() {}') {
+    ig.game.isEditorHere=editswit
+};
+editswit?ig.game[map].setMap=setmap23:ig.game[map].setMap=setmap22}, 0)
