b2cf665efc4449cb412e41548f2caf6556f1b2f1
a-128 <128114816+a-128@users.noreply.github.com>
2023-10-12 22:18:14 +0100

    Update redhaze.js

--- redhaze.js
+++ redhaze.js
@@ -6,6 +6,6 @@ rhsize=75
 rhon=false
 setInterval(()=>{
     if (rhon==true) {
-ig.game.attachmentManager[throwItem](ig.game.player, "h", true, {opp: {x:ig.game.player.pos.x+(Math.round(Math.random()*rhsize)-(rhsize/2))-9.5, y: ig.game.player.pos.y+(Math.round(Math.random()*rhsize)-(rhsize/2))-9.5}, opv: {x:ig.game.player[facingRight]?-rhvelx:rhvelx, y: rhvely}, tid: ig.game.player.O3263})
+ig.game.attachmentManager[throwItem](ig.game.player, "h", true, {opp: {x:ig.game.player.pos.x+(Math.round(Math.random()*rhsize)-(rhsize/2))-9.5, y: ig.game.player.pos.y+(Math.round(Math.random()*rhsize)-(rhsize/2))-9.5}, opv: {x:ig.game.player[facingRight]?-rhvelx:rhvelx, y: rhvely}, tid: ig.game.player[Deobfuscator.keyBetween(AimableManager.prototype.shoot, "c.tid=a.", "++;")]})
     }
 }, 50)
