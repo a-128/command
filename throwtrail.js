5a8d053e42eff8a2ffb3ca8720c047f43ed1a625
a-128 <128114816+a-128@users.noreply.github.com>
2023-10-12 22:17:33 +0100

    Update throwtrail.js

--- throwtrail.js
+++ throwtrail.js
@@ -5,6 +5,6 @@ vely=60;
 itemtrail=false;
 setInterval(()=>{
     if (ig.input.state("click")&&itemtrail==true) {
-        ig.game.attachmentManager[throwItem](ig.game.player, "h", true, {opp: {x:Math.round(ig.game.screen.x+ig.input.mouse.x), y: Math.round(ig.game.screen.y+ig.input.mouse.y)}, opv: {x:ig.game.player[facingRight]?-velx:velx, y: vely}, tid: ig.game.player.O3263})
+        ig.game.attachmentManager[throwItem](ig.game.player, "h", true, {opp: {x:Math.round(ig.game.screen.x+ig.input.mouse.x), y: Math.round(ig.game.screen.y+ig.input.mouse.y)}, opv: {x:ig.game.player[facingRight]?-velx:velx, y: vely}, tid: ig.game.player[Deobfuscator.keyBetween(AimableManager.prototype.shoot, "c.tid=a.", "++;")]})
     }
 }, 50)
